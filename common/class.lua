71650b2e73fb457bd3faa2bbe0c7b6ea33f4f5e0
Jasper den Ouden <o.jasper@gmail.com>
2015-10-29 01:46:21 +0100

    `:init` should be inherited

--- common/class.lua
+++ common/class.lua
@@ -10,15 +10,17 @@ end
 return function (name, ...)
    assert(type(name) == "string")
    local Class = {}
+
+   Class.init = init_fn
+
    for _, el in ipairs{...} do  -- Copy all derived-froms.
       for k,v in pairs(el) do Class[k] = v end
    end
 
+   Class.new  = new_fn
+
    Class.__index = Class
    Class.__name  = name
-
-   Class.new  = new_fn
-   Class.init = init_fn
 
    return Class
 end
